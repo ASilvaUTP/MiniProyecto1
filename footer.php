7ff3da73b8035c8b6d3b0a6bc5774f637ca167b3
asilva <alejandro.silva@utp.ac.pa>
2025-10-09 19:59:50 -0500

    Segundo commit: Arreglo de footer

--- footer.php
+++ footer.php
@@ -1,3 +1,3 @@
 <footer>
-    <p>&copy; <?php echo date("Y"); ?> - Mini Proyecto #2 | Fecha: <?php echo date("d/m/Y"); ?></p>
+    <p>&copy; <?php echo date("Y"); ?> - Mini Proyecto #1 | Fecha: <?php echo date("d/m/Y"); ?></p>
 </footer>
